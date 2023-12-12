004900b72943e6242410cc5c8fb470ff35d86028
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-12-12 17:35:36 +0900

    initialize contract

---
+++ contracts/Contract.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+// interfaces
+import "./interfaces/IAllo.sol";
+import "./interfaces/IRegistry.sol";
+import "./interfaces/IStrategy.sol";
+
+contract Contract is Initializable {
+    IAllo public allo;
+    IRegistry public registry;
+    IStrategy public strategy;
+
+    /// @dev see { openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol }
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        IAllo _allo,
+        IRegistry _registry,
+        IStrategy _strategy
+    ) public initializer {
+        allo = _allo;
+        registry = _registry;
+        strategy = _strategy;
+    }
+}
